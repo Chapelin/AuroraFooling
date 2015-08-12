8eead3e3319b5d20efb2a675d773f8a929c77116
Chapelin <antbichon@gmail.com>
2015-08-12 22:04:33 +0200

    added a tagreader sandbox project : it works !

---
+++ TagReader/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagLib;
+using File = System.IO.File;
+
+namespace TagReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"\\NAS-PERSO\Volume_1\Musique\Lamb of God\(2009) Wrath";
+
+            var list = Directory.EnumerateFiles(path, "*.mp3");
+            foreach (var file in list)
+            {
+                Stream s = System.IO.File.Open(file, FileMode.Open);
+                var fsa = new StreamFileAbstraction(Path.GetFileName(file), s, s);
+                var tagFile = TagLib.File.Create(fsa);
+
+                var tags = tagFile.GetTag(TagTypes.Id3v2);
+                Console.WriteLine();
+            }
+        }
+    }
+}
