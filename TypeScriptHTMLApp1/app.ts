ca513e248a736c6940909e0d0068be06af78dfbb
Antoine Bichon <antoinebichon@msn.com>
2015-08-16 22:34:23 +0200

    retour du projet angular

---
+++ TypeScriptHTMLApp1/app.ts
@@ -0,0 +1,39 @@
+﻿/// <reference path="Scripts/typings/angular2/angular2.d.ts" />
+/// <reference path="Scripts/Aurora.d.ts" />
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component(
+    {
+        selector: 'lecteur'
+    })
+@View({
+        templateUrl: 'Views/lecteur.html'
+})
+class MyAppComponent {
+    name: string;
+    currentPlayer: AV.Player;
+
+    constructor() {
+        this.name = "Toto";
+        this.currentPlayer = AV.Player.fromURL("music.flac");
+    }
+
+    public play() {
+        this.currentPlayer.play();
+    }
+
+    public pause() {
+        this.currentPlayer.pause();
+    }
+
+    public stop() {
+        this.pause();
+        this.currentPlayer.seek(0);
+    }
+
+
+
+}
+
+
+bootstrap(MyAppComponent);
