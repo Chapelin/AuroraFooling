a67c9ba3484b5b25e9783239e67402107d4c2e18
Antoine Bichon <antoinebichon@msn.com>
2015-08-17 20:17:34 +0200

    deplacement app.ts dans scripts

--- ServeurSide/Scripts/app.ts
+++ ServeurSide/Scripts/app.ts
@@ -1,9 +1,39 @@
-﻿
-var player: AV.Player;
-player = AV.Player.fromURL("http://localhost:50522/Content/music.flac");
+﻿/// <reference path="typings/angular2/angular2.d.ts" />
+/// <reference path="Aurora.d.ts" />
+import {Component, View, bootstrap} from 'angular2/angular2';
 
-function Launch(url: string) {
+@Component(
+    {
+        selector: 'lecteur'
+    })
+@View({
+        templateUrl: 'Views/lecteur.html'
+})
+class MyAppComponent {
+    name: string;
+    currentPlayer: AV.Player;
 
-    player = AV.Player.fromURL(url);
-    player.play();
+    constructor() {
+        this.name = "Toto";
+        this.currentPlayer = AV.Player.fromURL("music.flac");
+    }
+
+    public play() {
+        this.currentPlayer.play();
+    }
+
+    public pause() {
+        this.currentPlayer.pause();
+    }
+
+    public stop() {
+        this.pause();
+        this.currentPlayer.seek(0);
+    }
+
+
+
 }
+
+
+bootstrap(MyAppComponent);
