6c6a007fdcb2bef94229b667eb8b92f7320c6a19
Antoine Bichon <antoinebichon@msn.com>
2015-08-17 21:26:20 +0200

    ajout artistlist

---
+++ ServeurSide/Scripts/ArtistList.ts
@@ -0,0 +1,24 @@
+﻿/// <reference path="typings/angular2/angular2.d.ts" />
+import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
+@Component(
+    {
+        selector: 'artistlist'
+    })
+@View({
+        templateUrl: 'Views/artistList.html',
+        directives: [NgFor]
+})
+class ListOfArtists {
+    artists: string[];
+
+    constructor() {
+        this.artists = ["Test A", "Test b", "Test c"];
+    }
+
+   
+
+
+
+}
+
+bootstrap(ListOfArtists);
